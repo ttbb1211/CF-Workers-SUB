16c73932c3d96a7143014ad8fe286a608ecb02b0
ttbb1211 <48145747+ttbb1211@users.noreply.github.com>
2025-11-26 20:09:14 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ https://cfxr.eu.org/getSub
 
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/ttbb1211/clash/refs/heads/main/ACL4SSR_Online_Full.ini"; //订阅配置文件
+let subConfig = "https://raw.githubusercontent.com/ttbb1211/clash/refs/heads/main/ACL4SSR_Online.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
@@ -826,4 +826,5 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 		});
 	}
 }
+
 
